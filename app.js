5496403610cd27b41e943b30a74939968a1fc7e7
will <williampaulton@yahoo.uk>
2025-03-29 21:42:55 +0000

    update package.json

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get('/helloworld', (req, res) => {
   res.send('Hello, World!');
 });
 
+app.get('/', (req, res) => {
+  res.send('Hello, World! from default route');
+});
+
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
